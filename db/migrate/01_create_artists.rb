70fd124eb6aa2119501bfbc1627a610ae08eba57
github username <github email address>
2019-02-07 22:59:08 -0800

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration[5.1]
+
+  def up
+  end
+
+  def change
+    #create a table
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+  def down
+  end
+
+end
