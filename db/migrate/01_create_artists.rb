3fcbf2f7c48465cc21be9ab3c1029317f92ec45a
Tiffany <Tafby88@gmail.com>
2019-02-08 10:18:56 -0800

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,19 @@
+class CreateArtists < ActiveRecord::Migration
+
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+    end
+end
+
+
+
+    # def up
+    # end
+
+    # def down
+    # end
